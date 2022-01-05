6d0f1def0e518653683228461454d2978ab5d405
Dan Clissold <djclissold@yahoo.co.uk>
2022-01-05 19:37:19 +1100

    New commit

---
+++ GitHubActionsDemo/Services/ServiceOne.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace GitHubActionsDemo.Services
+{
+    public class ServiceOne
+    {
+        public ServiceOne()
+        {
+        }
+
+        public bool MethodOne(bool input)
+        {
+            return input;
+        }
+    }
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using GitHubActionsDemo.Services;
+using Xunit;
+
+namespace Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            var service = new ServiceOne();
+            var expected = true;
+
+            //Act
+            var result = service.MethodOne(expected);
+
+            //Assert
+            Assert.Equal(expected, result);
+            
+        }
+    }
+}
